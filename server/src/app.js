a8f023e7721cc0763c6fd6546db5b00c482acb15
Amandeep75 <Amandeep75318gmail.com>
2024-09-05 14:30:00 +0530

    add new frontend url

--- server/src/app.js
+++ server/src/app.js
@@ -6,11 +6,13 @@ const productRouter = require('./routes/product.router.js')
 
 
 const app = express()
-app.use(cors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:5173' ,
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
-    credentials:true
-}))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "https://cart-e-server.vercel.app",
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    credentials: true,
+  })
+);
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
