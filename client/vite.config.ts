3930df42b26894dc4b569194d09b50577de9c4c1
Amandeep75 <Amandeep75318gmail.com>
2024-09-05 14:24:19 +0530

    adding proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     '/v1': 'http://localhost:4000'
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/v1": "https://cart-e-server.vercel.app",
+    },
+  },
   plugins: [react()],
   build: {
     sourcemap: false, // Disable source maps
