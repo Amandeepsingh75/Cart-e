f8a96f8ae391b41d8070970b812239ca40430c7f
Amandeep75 <Amandeep75318gmail.com>
2024-09-05 15:05:55 +0530

    change app.cors

--- server/src/app.js
+++ server/src/app.js
@@ -6,13 +6,11 @@ const productRouter = require('./routes/product.router.js')
 
 
 const app = express()
-app.use(
-  cors({
-    origin:"*",
-    methods: "GET,POST,PUT,DELETE,OPTIONS",
-    credentials: true,
-  })
-);
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173' ,
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    credentials:true
+}))
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
